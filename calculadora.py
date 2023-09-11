b6bfc96ce3332e40d16e6263911b55a14814631d
neribg <buenon668@gmail.com>
2023-09-11 11:33:04 -0600

    mi primer programa que calcula

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+'''
+Autor: Felipe Neri Francisco Bueno González 
+Fecha: 11 septiembre 
+Describcion: iniciando con python 
+'''
+
+a = input("ingresa el primer valor ")
+b = input("ingresa el segundo valor ")
+print (type(a), type(b))
+print ("Suma ", a+b)
+a = int (a)
+b = int (b)
+print (type(a), type(b))
+print ("Suma ", a+b)
--- saludar.py
+++ saludar.py
@@ -1,2 +1,7 @@
-nombre = input("Ingresa tu nombre ")
-print ('hola  ', nombre )
+'''
+Autor: Felipe Neri Francisco Bueno González 
+Fecha: 11 septiembre 
+Describcion: iniciando con python 
+'''
+nombre = input("Ingresa tu nombre: ")
+print ('hola ', nombre )   # imprime variables 
