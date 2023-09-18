e2f587a4d34dae7a4ee922b8aa147b7b44252aa1
neribg <142611373+neribg@users.noreply.github.com>
2023-09-18 11:36:03 -0600

    Add files via upload

---
+++ laboratorio 2/laboratorio_2.1.1.18.py
@@ -0,0 +1,8 @@
+
+''''
+  Nombre: Felipe Neri Francisco Bueno González
+  Fecha: 18 de septiembre 2023
+  Descripción:laboratorio: 2.1.1.18
+'''
+print("Fundamentos","Programacion","en",sep="***", end="...")
+print("Python")
---
+++ laboratorio 2/laboratorio_2.1.1.6.py
@@ -0,0 +1,10 @@
+''''
+  Nombre: Felipe Neri Francisco Bueno González
+  Fecha: 18 de septiembre 2023
+  Descripción: laboratorio: 2.1.1.6
+'''
+
+print("Hola", 'mundo', sep="*")
+print()
+print("Bienbenido\n a python "+"Redes", end= "#")
+print("Diguitales "*3)
