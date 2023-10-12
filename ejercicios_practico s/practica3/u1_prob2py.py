703e6fa77393a7119a7b0a98ab4dada47e00295e
neribg <142611373+neribg@users.noreply.github.com>
2023-10-11 19:41:54 -0600

    Add files via upload

---
+++ ejercicios_practico s/practica3/u1_prob2py.py
@@ -0,0 +1,20 @@
+﻿﻿'''
+ Alumno: Felipe Neri Francisco Bueno gonzález
+ Fecha: 2 de octubre de 2023
+ Descripcion: No de lista impar Jugando con listas
+'''
+
+subjects = ["Probabilidad y Estadística", 
+"Electrónica para IDC Física", 
+"Conexión de redes WAN",
+"Administración de servidores I", 
+"Programación de Redes",
+"Inglés IV"]
+grades = []
+
+for subject in subjects:
+    grade = input("¿Cuál fue tu nota en la unidad I de " + subject + "? ")
+    grades.append(grade)
+
+for i in range(len(subjects)):
+    print("En " + subjects[i] + " has sacado " + grades[i])
